a37f16c684115582cc53e569a3558fb3822801be
Kuznetsova Kristina <101335982+smithy74@users.noreply.github.com>
2023-03-05 18:56:25 +0300

    updates files

--- logger.c
+++ logger.c
@@ -1,7 +1,4 @@
 #include "logger.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 void logger(const char* tag, const char* message) {
    time_t now;
--- logger.h
+++ logger.h
@@ -1,6 +1,12 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 void logger(const char* tag, const char* message);
+
+// #include "logger.c"
 
 #endif /* LOG_H */
